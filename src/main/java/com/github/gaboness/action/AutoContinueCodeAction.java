46f4ec8a7d1988715c77626473733b307595cc5c
wangjb26360 <wangjb26360@hundsun.com>
2024-02-01 15:03:27 +0800

    【修改单号】:T002 【修改说明】:Action类的demo

---
+++ src/main/java/com/github/gaboness/action/AutoContinueCodeAction.java
@@ -0,0 +1,29 @@
+package com.github.gaboness.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * 自动续写
+ * @Author gaboness
+ * @Date 2024/02/01 13:58:00
+ */
+public class AutoContinueCodeAction extends AnAction {
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        e.getPresentation().setEnabledAndVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        Project project = e.getProject();
+//        Project project1 = e.getData(PlatformDataKeys.PROJECT);
+        String text = e.getPresentation().getText();
+        String description = e.getPresentation().getDescription();
+        // 弹出消息框
+        Messages.showMessageDialog(project,description,text,Messages.getInformationIcon());
+    }
+}
